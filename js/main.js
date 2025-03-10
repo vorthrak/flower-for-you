dac997c500f368cf974c89876246676f64fc5ab7
vorthrak <elricodaru@gmail.com>
2025-03-10 11:43:56 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
   
-      const titles = ('BAIM sange sama  SHEREEN').split('')
+      const titles = ('I LOVE YOU').split('')
       const titleElement = document.getElementById('title');
       let index = 0;
   
